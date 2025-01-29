c86c58b721a4addcc5c5e1131a71e35f9ca4683e
Cauan Neves <c.neves8903@gmail.com>
2025-01-29 13:05:54 -0300

    0.1

--- interface.py
+++ interface.py
@@ -1,5 +1,10 @@
 import PySimpleGUI as sg
 
+#Alterando cursor
+def set_cursor(element, cursor):
+    widget= element.Widget
+    widget.configure(cursor=cursor)
+
 #Tema
 sg.theme('LightBlue3')
 
@@ -7,13 +12,19 @@ sg.theme('LightBlue3')
 layout= [
     [sg.Text('Usuário:')],
     [sg.Input(key= 'login')],
+    [sg.Text(size= 0, key= 'error_login')],
     [sg.Text('Senha:')],
     [sg.Input(key= 'password')],
-    [sg.Button(button_text='Login', expand_x= True), sg.Button(button_text= 'Sign up!',  expand_x= True)]
+    [sg.Text(size= 0, key= 'error_password')],
+    [sg.Button(button_text='Entrar', key= 'login', size= 10)],
+    [sg.Text('Não possui cadastro? Clique aqui!', enable_events= True, expand_x= True, key= 'sign_up', text_color= 'blue', font= ('Helvetica', 10, 'underline'))]
 ]
 
 #Janela
-window= sg.Window('Tela', layout= layout)
+window= sg.Window('Tela', layout= layout, finalize= True)
+
+#Alterando o cursor
+set_cursor(window['sign_up'], 'hand2')
 
 #leitura
 while True:
@@ -21,7 +32,8 @@ while True:
     #Ler e reagir aos eventos
     if event == sg.WIN_CLOSED:
         break
-    elif event == 'OK':
-        print('Clicou no botão "OK"')
+    elif event == 'login':
+        print('Clicou no botão "Login"')
+    elif event == 'sign_up':
+        print('Mover para área de cadastro')
     print(values)
-    
