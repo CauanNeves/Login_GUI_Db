d24ed313c3ae915021c6e6ff53ccf434c25d72a6
Cauan Neves <c.neves8903@gmail.com>
2025-01-30 15:18:46 -0300

    0.1

--- interface.py
+++ interface.py
@@ -5,42 +5,51 @@ def set_cursor(element, cursor):
     widget= element.Widget
     widget.configure(cursor=cursor)
 
-#Tema
-sg.theme('LightBlue3')
-
 #Layouts
-#Janela login
-def window_login():
-    layout= [
-        [sg.Text('Login', expand_x= True ,justification= 'c')],
-        [sg.Text('Usuário:')],
-        [sg.Input(key= 'login', expand_x= True)],
-        [sg.Text('Senha:')],
-        [sg.Input(key= 'password', expand_x= True)],
-        [sg.Button(button_text='Entrar', key= 'login', expand_x= True)],
-        [sg.Text('Não possui cadastro? Clique aqui!', enable_events= True, expand_x= True, key= 'sign_up', text_color= 'blue', font= ('Helvetica', 10, 'underline'))]
-    ]
-    return sg.Window('Login', layout= layout, finalize= True)
+def main():
+    #Janela login
+    def window_login():
+        layout= [
+            [sg.Text('Login', expand_x= True ,justification= 'c')],
+            [sg.Text('Usuário:')],
+            [sg.Input(key= 'login', expand_x= True)],
+            [sg.Text('Senha:')],
+            [sg.Input(key= 'password', expand_x= True)],
+            [sg.Button(button_text='Entrar', key= 'login', expand_x= True)],
+            [sg.Text('Não possui cadastro? Clique aqui!', enable_events= True, expand_x= True, key= 'sign_up', text_color= 'blue', font= ('Helvetica', 10, 'underline'))]
+        ]
+        return sg.Window('Login', layout= layout, finalize= True)
 
-#Janela Cadastro
-def window_sign_up():
-    layout=[
-        [sg.Text('Está vai ser a tela de cadastro do usuário')]
-    ]
-    return sg.Window('Cadastrar', layout= layout, finalize= True)
+    #Janela Cadastro
+    def window_sign_up():
+        layout=[
+            [sg.Input('Nome de Usuário', expand_x= True, key= '-user-')],
+            [sg.Input('Senha', expand_x= True)],
+            [sg.Button('Cadastrar', expand_x= True)]
+        ]
 
-window_login_, window_sign_up_ = window_login(), None
+        window = sg.Window('Cadastrar', layout= layout,
+                            finalize= True,
+                            auto_size_text=False,
+                            default_element_size=(20, 1),
+                            text_justification='r',
+                            return_keyboard_events=True,
+                            grab_anywhere=False)
 
-#Alterando o cursor
-set_cursor(window_login_['sign_up'], 'hand2')
+        #leitura
+        while True:
+            event, values= window.read()
+            #Ler e reagir aos eventos
+            if event is None:
+                break
+            elif event == sg.WIN_CLOSED:
+                break
+            elif values['-user-'] == 'usuario':
+                print('Já existe esse nome de usuário')
 
-#leitura
-while True:
-    window, event, values= sg.read_all_windows()
-    #Ler e reagir aos eventos
-    if event == sg.WIN_CLOSED:
-        break
-    if window == window_login_ and event == 'sign_up':
-        window_sign_up_ = window_sign_up()
-        window_login_.hide() #.un_hide() para exibir novamente a janela
-    print(values)
+
+    window_sign_up()
+
+if __name__ == '__main__':
+    sg.theme('LightBlue3')
+    main()
